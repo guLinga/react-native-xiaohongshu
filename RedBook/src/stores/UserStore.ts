5f60983606631ca0cee40d9c22d097cbbd7761d1
GuLing <2634917964@qq.com>
2023-04-22 21:27:07 +0800

    refactor：使用mobx重构userStore

--- RedBook/src/stores/UserStore.ts
+++ RedBook/src/stores/UserStore.ts
@@ -1,21 +1,30 @@
+import {flow} from 'mobx'
 import { login } from '../api/apis';
-
+import { save } from '../utils/Storage';
 
 class UserStore {
 
   userInfo: any
 
-  requestLogin = async (name:string,pwd:string,callback:(success:boolean)=>void) => {
+  requestLogin = flow(function* (
+    this:UserStore, 
+    name:string,
+    pwd:string,
+    callback:(success:boolean)=>void
+  ){
     try {
-      const { data } = await login(name,pwd);
+      const { data } = yield login(name,pwd);
+      // 储存登录信息
+      save('userInfo',JSON.stringify(data));
       this.userInfo = data || null;
-      callback?.(data?true:false)
+      callback?.(data?true:false);
     } catch (error) {
       console.log(error);
       this.userInfo = null;
       callback?.(false);
     }
-  }
+  })
+
 }
 
 // ES6单例导出
