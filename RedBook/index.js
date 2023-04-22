cbef9fa7beadf915920259f4da5e6b0264d0dce7
GuLing <2634917964@qq.com>
2023-04-22 17:16:48 +0800

    feat：给安卓开启LayoutAnimation功能

--- RedBook/index.js
+++ RedBook/index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform, UIManager} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+// 给安卓开启LayoutAnimation功能
+if(Platform.OS === 'android'){
+  if(UIManager.setLayoutAnimationEnabledExperimental){
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
 
 AppRegistry.registerComponent(appName, () => App);
